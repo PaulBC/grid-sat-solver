e0d5f1687d26113d9d3a0085f98341e07a8b3c5f
Paul Callahan <callahanpb@gmail.com>
2021-01-18 13:16:59 -0800

    implemented hex tiling with symmetry

--- displayhex.py
+++ displayhex.py
@@ -1,13 +1,13 @@
 from math import cos, sin, radians
 import turtle
 
-UNITLENGTH = 20
+UNITLENGTH = 15
 
 # in other words, sqrt(3)/2 and 1/2, but this makes the purpose clearer.
 XSCALE = cos(radians(60))
 YSCALE = sin(radians(60))
 
-# a stack to restore state after drawing 
+# a stack to restore state after drawing
 TURTLE_STACK = []
 
 def position(i, j):
@@ -32,7 +32,7 @@ def draw_hex(fillcolor=None):
     turtle.fillcolor(fillcolor)
   side = UNITLENGTH * XSCALE / YSCALE
   turtle.penup()
-  turtle.left(90) 
+  turtle.left(90)
   turtle.forward(side)
   turtle.right(120)
   turtle.pendown()
@@ -68,13 +68,13 @@ def draw_rhombus(rotation, fillcolor=None):
   turtle.penup()
   pop_state()
 
-RHOMBUS_COLORS = ['lightblue', 'firebrick', 'lightgreen'] 
+RHOMBUS_COLORS = ['lightblue', 'firebrick', 'lightgreen']
 def rhombus_cell(i, j, rotation):
-  push_state() 
+  push_state()
   (x, y) = position(i, j)
   turtle.setposition(turtle.xcor() + x, turtle.ycor() + y)
   draw_rhombus(rotation * 120, RHOMBUS_COLORS[rotation])
-  pop_state() 
+  pop_state()
 
 def draw_rhombus_cells(xorigin, yorigin, cells):
   turtle.tracer(0, 0)
@@ -87,5 +87,28 @@ def draw_rhombus_cells(xorigin, yorigin, cells):
     for j in range(len(cells[i])):
       if cells[i][j] is not None:
         rhombus_cell(i, j, cells[i][j])
+  pop_state()
+  turtle.update()
+
+HEX_COLORS = ['black', 'tan']
+def hex_cell(i, j, value):
+  push_state()
+  (x, y) = position(i, j)
+  turtle.setposition(turtle.xcor() + x, turtle.ycor() + y)
+  draw_hex(HEX_COLORS[value])
+  pop_state()
+
+def draw_hex_cells(xorigin, yorigin, cells):
+  turtle.tracer(0, 0)
+  turtle.hideturtle()
+  turtle.speed(0)
+  push_state()
+  turtle.penup()
+  turtle.setposition(xorigin, yorigin)
+  turtle.pencolor('#8080ff')
+  for i in range(len(cells)):
+    for j in range(len(cells[i])):
+      if cells[i][j] is not None:
+        hex_cell(i, j, cells[i][j])
   pop_state()
   turtle.update()
---
+++ testing/testhextile.py
@@ -0,0 +1,70 @@
+import sys
+
+from gridbuilder import *
+from tesselation import *
+from clausebuilder import *
+from displayhex import *
+from rulesymmetry import *
+from dimacs_sat import *
+from solver import *
+
+def make_rotated_clauses(clause):
+  return all_symmetries(parse_line(clause), ROTATED_HEX)
+
+def make_totalistic_clauses(clause):
+  return all_symmetries(parse_line(clause), TOTALISTIC_HEX)
+
+dimacs_file = sys.argv[1] + '.dim'
+symbolic_file = sys.argv[1] + '.sym'
+solution_file = sys.argv[1] + '.out'
+
+print('# generating clauses for tiling rule')
+# birth on zero or 2m
+birth_0 = [parse_line('G <- ~NW ~N ~E ~SE ~S ~W')]
+birth_2o = make_rotated_clauses('G <- NW N ~E ~SE ~S ~W')
+# survival on 2m
+survive_2m_a = make_rotated_clauses('G <- O NW ~N E ~SE ~S ~W')
+survive_2m_b = make_rotated_clauses('~G <- ~O NW ~N E ~SE ~S ~W')
+# death on 1-, 2o, 2p, 3+
+death_le_1 = make_rotated_clauses('~G <- ~N ~E ~SE ~S ~W')
+death_2o = make_rotated_clauses('~G <- NW N ~E ~SE ~S ~W')
+death_2p = make_rotated_clauses('~G <- NW ~N ~E SE ~S ~W')
+death_ge_3 = make_totalistic_clauses('~G <- NW N E')
+all_constraints = birth_0 + birth_2o + survive_2m_a + survive_2m_b + death_le_1 + death_2o + death_2p + death_ge_3
+print('# done')
+
+# make CA grid
+root = MooreGridNode((0, 0, 0), Tesselated(RotatedRhombus(15)), PeriodicTimeAdjust(1, 0, 0))
+grid = build_grid(root)
+
+# create clauses for life rule conditions on grid
+clauses = inflate_grid_template(all_constraints, grid, G.name)
+
+# add clauses for a cardinality bound of >= 12 in first generation
+clauses.extend(bound_population(grid, GreaterThanOrEqual, 12))
+
+# write dimacs file for solver
+with open(dimacs_file, 'w') as out:
+  output_dimacs(clauses, out)
+
+# write symbolic form of clauses
+with open(symbolic_file, 'w') as out:
+  output_symbolic(clauses, out)
+
+# solve and print results
+results = solve(dimacs_file, solution_file)
+valuegrid = get_value_grid('c', results)
+
+cells = []
+for i in range(len(valuegrid[0]) * 3):
+  row = []
+  for j in range(len(valuegrid[0][0]) * 3):
+    it, jt, _ = root.equivalence.to_equivalent(i, j)
+    row.append(valuegrid[0][it][jt])
+  cells.append(row)
+
+draw_hex_cells(-200, 200, cells)
+
+width = len(cells[0])
+for i in range(len(cells)):
+  print(" " * (width - i), " ".join(['O' if x else '.' for x in cells[i]]))
