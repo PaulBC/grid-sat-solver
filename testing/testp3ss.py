834ce7a1d724c000408dc83a7a85efc5def2f8ea
Paul Callahan <callahanpb@gmail.com>
2021-01-18 09:11:45 -0800

    oops forgot to check in

---
+++ testing/testp3ss.py
@@ -0,0 +1,49 @@
+import sys
+
+from gridbuilder import *
+from tesselation import *
+from clausebuilder import *
+from rulesymmetry import *
+from dimacs_sat import *
+from solver import *
+
+def make_clauses(clause):
+  return all_symmetries(parse_line(clause), TOTALISTIC)
+
+dimacs_file = sys.argv[1] + '.dim'
+symbolic_file = sys.argv[1] + '.sym'
+solution_file = sys.argv[1] + '.out'
+
+print('# generating clauses for CA rule')
+lt2 = make_clauses('~G <- ~N ~NE ~E ~SE ~S ~SW ~W')
+ge4 = make_clauses('~G <- N NE E SE')
+eq3 = make_clauses('G <- N NE E ~SE ~S ~SW ~W ~NW')
+eq2a = make_clauses('~G <- ~O N NE ~E ~SE ~S ~SW ~W ~NW')
+eq2b = make_clauses('G <- O N NE ~E ~SE ~S ~SW ~W ~NW')
+all_constraints = lt2 + ge4 + eq3 + eq2a + eq2b
+print('# done')
+
+root = MooreGridNode((0, 0, 0), Open(5, 16), PeriodicTimeAdjust(3, 1, 0))
+grid = build_grid(root)
+
+# create clauses for life rule conditions on grid
+clauses = inflate_grid_template(all_constraints, grid, G.name)
+
+# add clauses for a cardinality bound of == 25 in first generation
+clauses.extend(bound_population(grid, GreaterThanOrEqual, 25))
+clauses.extend(bound_population(grid, LessThanOrEqual, 25))
+
+# write dimacs file for solver
+with open(dimacs_file, 'w') as out:
+  output_dimacs(clauses, out)
+
+# write symbolic form of clauses
+with open(symbolic_file, 'w') as out:
+  output_symbolic(clauses, out)
+
+# solve and print results
+results = solve(dimacs_file, solution_file)
+valuegrid = get_value_grid('c', results)
+
+for i in range(len(valuegrid[0])):
+  print("".join(['*' if x else '.' for x in valuegrid[0][i]]))
