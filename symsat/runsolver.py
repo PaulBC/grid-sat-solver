4e911c7443103c840d045232e47ea790913efccf
PaulBC <callahanpb@gmail.com>
2022-03-05 11:01:50 -0800

    Update runsolver.py
    added option to show only true values and set seed

--- symsat/runsolver.py
+++ symsat/runsolver.py
@@ -3,7 +3,7 @@ import sys
 from .dimacs_sat import parse_line, output_dimacs
 from .solver import solve, set_solver
 
-def solve_and_print(input_file, echo=False):
+def solve_and_print(input_file, echo=False, showall=False, seed=None):
   input_root = os.path.splitext(input_file)[0]
   dimacs_file = input_root + '.dim'
   solution_file = input_root + '.out'
@@ -17,14 +17,18 @@ def solve_and_print(input_file, echo=False):
     output_dimacs(clauses, out)
 
   # solve and print results
-  results = solve(dimacs_file, solution_file, None, echo)
+  results = solve(dimacs_file, solution_file, seed, echo)
 
   print('Assignments to values are:')
   for key, value in results:
-    if key <= '{':
+    if key <= '{' and (showall or value):
       print('%s %s' % (key, value))
 
 if __name__ == "__main__":
   if len(sys.argv) > 2:
     set_solver(sys.argv[2])
-  solve_and_print(sys.argv[1], True)
+
+  seed = int(sys.argv[3]) if len(sys.argv) > 3 else None
+
+  solve_and_print(sys.argv[1], True, seed=seed)
+  
